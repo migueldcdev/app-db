da24e607c3b6f648af4dd6eb5947769a5ab42e27
migueldc <migueldc.dev@protonmail.com>
2022-02-28 15:55:40 +0100

    Latest commit db-setup

--- server/app.js
+++ server/app.js
@@ -1,51 +1,28 @@
 const express = require('express');
-const mariadb = require('mariadb');
 const app = express();
+const db = require('mariadb/callback');
 
-
-
-const pool = mariadb.createPool({
-    port:3306,
-    host: '',
+const pool = db.createPool({
+    host: '',      
     user:'',
     password: '',
-    connectionLimit: 5,
-    database: "app"
+    database: 'todo'
 })
 
-async function asyncFunction() {
-    let conn;
-    //const insert = 'INSERT INTO todo (task) VALUES ("Do the laundry")' 
-    try {
-      //pool.query(insert, (err,))
-      conn = await pool.getConnection();
-      console.log(conn);
-      //const rows = await conn.query("SELECT * FROM todo");
-      //console.log(rows); //[ {val: 1}, meta: ... ]
-      //const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-      
-      console.log(rows); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-  
-    } catch (err) {
-      throw err;
-    } finally {
-      if (conn) return conn.end();
-    }
-  }
-
-  asyncFunction();
-
-
-
-/*app.get('/',(req, res) => {
-    const insert = 'INSERT INTO todo (task) VALUES ("Do the laundry")';
-    
-    pool.query(insert, (err,result) => {
-    res.send("Database updated")
-});
-    
-})*/
+app.get('/', (req,res) => {
+    const insert = 'SELECT * FROM todo;'; 
+    pool.query(insert, (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);          
+        }
+        }
+      );
+    });
+ 
 
 app.listen(3001, () => {
-    console.log("Running...");
+    console.log(`App listening at http://localhost:3001`);    
 })
+ 
